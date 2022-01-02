33eed5407cb4923ef28afc7894d8406ff699c0bf
bnorman21 <bennett.norman321@gmail.com>
2022-01-01 16:39:26 -0800

    updating

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask
-from datetime import datetime
-from flask import render_template
-from flask import render_template
-import re
+from os import removedirs
+from flask import Flask, redirect, url_for, render_template, session, flash
+from flask import request
+from datetime import timedelta  
 #!python
-import mlbgame
 
 app = Flask(__name__)
+app.secret_key = "secret key"
+app.permanent_session_lifetime = timedelta(minutes=5)
 
-@app.route("/")
+@app.route("/", methods = ["POST", "GET"])
 def home():
     return render_template("home.html")
 
@@ -17,22 +17,67 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route("/contact/")
-def contact():
-    return render_template("contact.html")
 
-@app.route("/games/<name>/<team>/<month>/<year>")
-def hello_there(name = None, team = None, month = None, year = None):
+@app.route("/login/", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user  = request.form['nm']
+        session["user"] = user
+        return redirect (url_for("user"))
+    else:
+        if "user" in session:
+            return redirect (url_for("user"))
+        else:
+            return render_template("login.html")
+
+@app.route("/logout/")
+def logout():
+    if "user" in session:
+        user = session["user"]
+        flash (f"You have been logged out, {user}", "info")
+    session.pop("User", None)
+    return redirect(url_for("login "))
+
+@app.route("/user/")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template(
+            "user.html",
+            usr = user
+        )
+    else:
+        return redirect(url_for("login"))
+
+
+@app.route("/make-quiz/", methods=["POST", "GET"])
+def make_quiz():
+    if "user" in session:
+        if request.method == "POST":
+            quiz_name = request.form['qn']
+            num_questions = request.form.get('num_q', type=int)
+            return redirect(url_for('create_questions', qzn=quiz_name, num_q = num_questions))
+        else:
+            return render_template("make_quiz.html")
+    else:
+        return redirect(url_for("login"))
+
+@app.route("/<qzn>/<num_q>/")
+def create_questions(qzn, num_q: int):
     return render_template(
-        "games.html",
-        name=name,
-        team=team,
-        month=month,
-        year=year,
-        games = mlbgame.combine_games(mlbgame.games(int(year), int(month), home=team)),
-        date=datetime.now()
+        "create_questions.html",
+        qzn = qzn,
+        num_q = num_q,
+        usr = session["user"]
     )
 
-@app.route("/api/data")
+
+@app.route("/take-quiz/")
+def take_quiz():
+    return render_template("take_quiz.html")
+    
+
+@app.route("/api/")
 def get_data():
     return app.send_static_file("data.json")
