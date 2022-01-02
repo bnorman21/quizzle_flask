76881e7008233180f1fc14c28dff2cd3415072b2
bnorman21 <bennett.norman321@gmail.com>
2022-01-01 23:49:58 -0800

    basic notes version complete

--- website/static/index.js
+++ website/static/index.js
@@ -1,8 +1,10 @@
 function deleteNote(noteId) {
+  //to send a request in javascript use fetch
   fetch("/delete-note", {
     method: "POST",
     body: JSON.stringify({ noteId: noteId }),
   }).then((_res) => {
+    //reload window
     window.location.href = "/";
   });
 }
