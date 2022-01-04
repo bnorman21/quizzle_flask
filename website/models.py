916a09fc10946950783a593343f0b192afb382be
bnorman21 <bennett.norman321@gmail.com>
2022-01-04 08:13:30 -0800

    trying to update database model

--- website/models.py
+++ website/models.py
@@ -10,6 +10,51 @@ class Note(db.Model):
     #when linking the foreign key, remeber it is lower case
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+class Quiz(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quiz_name = db.Column(db.String(150))
+    quiz_category = db.Column(db.String(150))
+    #many quizzes to one relationship
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    #one quiz to many relationship
+    questions = db.relationship('Question')
+    question_scores = db.relationship('QuestionScore')
+    quiz_scores = db.relationship('QuizScore')
+
+class QuizScore(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    result = db.Column(db.Float)
+    #many quiz socres to one relationship
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    prompt = db.Column(db.String(150))
+    #starting with hardcoding four option then will work on implementing an array
+    option1 = db.Column(db.String(150))
+    option2 = db.Column(db.String(150))
+    option3 = db.Column(db.String(150))
+    option4 = db.Column(db.String(150))
+    correct_option = db.Column(db.String(150))
+    #many questions to one relationship
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    #one question to many relationship
+    question_scores = db.relationship('QuestionScore')
+
+class QuestionScore(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    correct = db.Column(db.Boolean)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
+    
+
+
+
 class User(db.Model, UserMixin):
     #need primary key
     id = db.Column(db.Integer, primary_key=True)
@@ -18,3 +63,7 @@ class User(db.Model, UserMixin):
     first_name = db.Column(db.String(150))
     #when you are creating the relationship, you state the name of the class
     notes = db.relationship('Note')
+    quizzes = db.relationship('Quiz')
+    quiz_scores = db.relationship('QuizScore')
+    questions = db.relationship('Question')
+    question_scores = db.relationship('QuestionScore')
